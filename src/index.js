41e7dfb993cb8fdcf769b4a4887dd7c582d012b7
John Simms <johnsi@tfg.co.za>
2021-11-09 10:07:17 +0200

    remove Switch import

--- src/index.js
+++ src/index.js
@@ -1,28 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store, history } from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
-import { ConnectedRouter } from 'connected-react-router'
-import { Route, Switch } from 'react-router' // react-router v4/v5
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { store, history } from "./app/store";
+import { Provider } from "react-redux";
+import * as serviceWorker from "./serviceWorker";
+import { ConnectedRouter } from "connected-react-router";
+import { Route } from "react-router";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store()}>
       <ConnectedRouter history={history}>
         <>
-          <Switch>
-            <Route exact path="/" render={() => (<App />)} />
-            <Route exact path="/test" render={() => (<><h1>test</h1></>)} />
-            <Route render={() => (<div>Miss</div>)} />
-          </Switch>
+          <Route exact path="/" render={() => <App />} />
+          <Route
+            exact
+            path="/test"
+            render={() => (
+              <>
+                <h1>test</h1>
+              </>
+            )}
+          />
+          <Route render={() => <div>Miss</div>} />
         </>
       </ConnectedRouter>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
